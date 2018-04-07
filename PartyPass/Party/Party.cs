dcfa23f2ea71ff3525cbb64e4ef6559b900c0133
Matan Diamond <mattdiamond98@gmail.com>
2018-04-07 18:35:23 -0400

    issaparty

--- PartyPass/Party/Party.cs
+++ PartyPass/Party/Party.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyPass.UserTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,15 @@ using System.Threading.Tasks;
 namespace PartyPass.Party {
     class Party {
 
+        public List<Bartender> Bartenders { get; set; }
+        public List<Bouncer> Bouncers { get; set; }
+
+        public Party(List<Bartender> bartenders, List<Bouncer> bouncers) {
+            Bartenders = bartenders;
+            Bouncers = bouncers;
+            if (Bartenders == null) Bartenders = new List<Bartender>();
+            if (Bouncers == null) Bouncers = new List<Bouncer>();
+        }
 
 
     }
