74ab3c4e08d5236cf6dad99fef5d73891faf8c24
Jordan Goldstein <JordanGoldstein@gatech.edu>
2018-04-07 16:51:04 -0400

    Adding User Classes

---
+++ PartyPass/UserTypes/IUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyPass.UserTypes
+{
+    /**
+     * Interface for all users of the application:
+     * Bouncers
+     * Bartenders
+     * Attendees
+     */
+    interface IUser
+    {
+        /*
+         * Returns the ID of the User
+         */
+        String getID();
+        /**
+         * Returns the type of the User
+         */
+        String getType();
+    }
+}
---
+++ PartyPass/UserTypes/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyPass.UserTypes
+{
+    /**
+     * Represents a User object
+     */
+    public abstract class User : IUser
+    {
+        enum types {Bouncer, Bartender, Attendee };
+        private String ID = "123456789";
+        private 
+        
+        String getID()
+        {
+            return ID;
+        }
+        String getType()
+        {
+            return type;
+        }
+
+    }
+}
